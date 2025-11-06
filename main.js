053358a5e1f4009e842bdb5daefeb35b6f0a6c81
anthony-murè <tony.mure99@gmail.com>
2025-11-06 15:39:47 +0100

    [add]snack-1

--- main.js
+++ main.js
@@ -7,3 +7,19 @@ Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise c
 BONUS: Ottieni l'intero post con l'autore
 Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
  */
+
+function getPostTitle(id) {
+
+  return new Promise((resolve, reject) => {
+
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then(res => res.json())
+      .then(post => resolve(post.title))
+      .catch(reject)
+
+  });
+};
+
+getPostTitle(1)
+  .then(title => console.log('Titolo del post:', title))
+  .catch(err => console.error(err));
