1c3b37e6a6ebab203a94e5b580f75250c59b42c7
anthony-murè <tony.mure99@gmail.com>
2025-11-06 15:27:30 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/*SNACK 1
+
+Ottieni il titolo di un post con una Promise.
+
+Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}.
+
+BONUS: Ottieni l'intero post con l'autore
+Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}.
+ */
