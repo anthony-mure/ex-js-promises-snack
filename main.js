d9a20acdc86e3cae800730cdc4c2617b482380ed
anthony-murè <tony.mure99@gmail.com>
2025-11-06 15:52:14 +0100

    [add]Bonus-snack-1

--- main.js
+++ main.js
@@ -23,3 +23,27 @@ function getPostTitle(id) {
 getPostTitle(1)
   .then(title => console.log('Titolo del post:', title))
   .catch(err => console.error(err));
+
+/*BONUS: Ottieni l'intero post con l'autore
+Crea una funzione getPost(id) che recupera l'intero post. Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore, recuperati dalla chiamata https://dummyjson.com/users/{post.userId}. */
+
+function getPost(id) {
+
+  return new Promise((resolve, reject) => {
+
+    fetch(`https://dummyjson.com/posts/${id}`)
+      .then(res => res.json())
+      .then(post => {
+        fetch(`https://dummyjson.com/users/${post.userId}`)
+          .then(res => res.json())
+          .then(user => resolve({ ...post, user }))
+          .catch(reject)
+      })
+      .catch(reject)
+
+  });
+};
+
+getPost(1)
+  .then(post => console.log('Post completo:', post))
+  .catch(err => console.error(err));
